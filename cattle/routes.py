aa851bf1b12cd17b90e1ead0757f778e55740a23
unknown <arjuncm1997@gmail.com>
2021-01-07 14:14:08 +0530

    qwe

---
+++ cattle/routes.py
@@ -0,0 +1,9 @@
+from flask import Flask, render_template
+from cattle import app
+
+
+
+
+@app.route('/')
+def index():
+    return render_template("index.html")
---
+++ run.py
@@ -0,0 +1,4 @@
+from cattle import app
+
+if __name__ == "__main__":
+    app.run(debug=True)
